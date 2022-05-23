e73d5949e2468841e7f8d7e48635476f6961a342
blubinskyj <blubinskyj@gmail.com>
2022-05-23 18:19:39 +0300

    fix links

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -10,23 +10,31 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=486fea3b&s=matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=486fea3b&s=matrix`)
       .then((response) => response.json())
       .then((data) => {
         this.setState({ movies: data.Search, loading: false });
+      })
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
       });
   }
 
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=486fea3b&s=${str}${
+      `https://www.omdbapi.com/?apikey=486fea3b&s=${str}${
         type != "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
       .then((data) => {
         this.setState({ movies: data.Search, loading: false });
+      })
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
       });
   };
 

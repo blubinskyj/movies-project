c9e9adcccb1cc12fa12f40f0e51d12acfecd30e7
blubinskyj <blubinskyj@gmail.com>
2022-05-23 13:33:55 +0300

    Clear project. Add materialise. Add basic layouts

---
+++ src/layout/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <nav className={"deep-purple darken-1"}>
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          React movies
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="#">Repo</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export { Header };
---
+++ src/layout/Main.jsx
@@ -0,0 +1,4 @@
+function Main() {
+  return <main className={"container content"}>Hello form movies app</main>;
+}
+export { Main };
